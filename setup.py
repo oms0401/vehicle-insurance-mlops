23e405bcf0988066f86e6d9da4900a53f83a64e9
Om Manoj Sharma <oms0401vns@gmail.com>
2025-02-05 01:42:12 +0530

    the final pipeline

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Om Manoj Sharma",
+    author_email="oms0401vns@gmail.com",
+    packages=find_packages()
+)
